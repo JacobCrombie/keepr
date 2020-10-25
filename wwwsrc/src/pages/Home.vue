3903addfae82b6d5c6a222f5bacae941cf0f6100
Darbyblinko <jacobcrombie@live.com>
2020-10-24 18:05:54 -0600

    modals

--- wwwsrc/src/pages/Home.vue
+++ wwwsrc/src/pages/Home.vue
@@ -2,33 +2,93 @@
   <div class="home-page container-fluid">
     <div class="row">
       <h1 class="text-center col">Welcome to Keepr</h1>
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-toggle="modal"
+        data-target="#exampleModalCenter"
+      >
+        Add New Keep
+      </button>
     </div>
 
+    <button class="btn btn-primary" @click="toggleModal">Add New Keep</button>
+    <div class="row"></div>
+
     <div class="row">
-      <form class="col" @submit.prevent="createKeep">
-        <div class="form-group-inline col-6">
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Name..."
-            v-model="keepData.name"
-          />
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Description..."
-            v-model="keepData.description"
-          />
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Image Url..."
-            v-model="keepData.img"
-          />
-          <button class="btn btn-info" type="submit">Add Keep</button>
+      <!-- Button trigger modal -->
+
+      <!-- Modal -->
+      <div
+        class="modal fade"
+        id="exampleModalCenter"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLongTitle">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form class="col" @submit.prevent="createKeep">
+                <div class="form-group-inline">
+                  <h4 class="text-center">Add A Keep</h4>
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Name..."
+                    v-model="keepData.name"
+                  />
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Description..."
+                    v-model="keepData.description"
+                  />
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Image Url..."
+                    v-model="keepData.img"
+                  />
+                  <button class="btn btn-info" type="submit">Add Keep</button>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                class="btn btn-primary"
+                data-dismiss="modal"
+              >
+                Save changes
+              </button>
+            </div>
+          </div>
         </div>
-      </form>
+      </div>
     </div>
+
     <div class="row justify-content-around">
       <keep-component v-for="keep in keeps" :key="keep.id" :keepProp="keep" />
     </div>
@@ -46,6 +106,7 @@ export default {
   data() {
     return {
       keepData: {},
+      toggleFormModal: false,
     };
   },
   computed: {
@@ -58,6 +119,7 @@ export default {
       this.$store.dispatch("createKeep", this.keepData);
       this.keepData = {};
     },
+    toggleModal() {},
   },
   components: { keepComponent },
 };
