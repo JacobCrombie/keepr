c9dce9c01ae0f141af88fa723a3c584fc597714f
Darbyblinko <jacobcrombie@live.com>
2020-10-26 13:25:42 -0600

    starting on vaultkeeps

---
+++ Controllers/VaultKeepsController.cs
@@ -0,0 +1,19 @@
+using Keepr.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keepr.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VaultKeepsController : ControllerBase
+    {
+        private readonly VaultKeepsService _vaultKeepServ;
+
+    public VaultKeepsController(VaultKeepsService vaultKeepServ)
+    {
+      _vaultKeepServ = vaultKeepServ;
+    }
+    [HttpGet]
+
+  }
+}
